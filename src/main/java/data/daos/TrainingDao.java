3f520f308c86dd5fca0580407152474204e42ed0
LigiaCano <ligiacanorengifo@gmail.com>
2016-03-13 23:24:56 +0100

    Persistence training

---
+++ src/main/java/data/daos/TrainingDao.java
@@ -0,0 +1,22 @@
+package data.daos;
+
+import java.util.Calendar;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import data.entities.Court;
+import data.entities.Training;
+import data.entities.User;
+
+public interface TrainingDao extends JpaRepository<Training, Integer> {
+	
+	List<Training> findByTrainer(User trainer);
+	
+	List<Training> findByCourt(Court court);
+	
+	List<Training> findByTrainerAndCourt(User trainer, Court court);
+	
+	Training findByTrainerAndStartDate(User trainer, Calendar date);
+
+}
---
+++ src/main/java/data/entities/Training.java
@@ -0,0 +1,119 @@
+package data.entities;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Training {
+	
+	@Id
+	@GeneratedValue
+	private int id;
+
+	@ManyToOne
+	@JoinColumn
+	private User trainer;
+	
+	@ManyToOne
+    @JoinColumn
+    private Court court;
+	
+	@ManyToMany(fetch = FetchType.EAGER)
+	private List<User> players;
+	
+	private Calendar startDate;
+	
+	private Calendar endDate;
+	
+	private int numberOfPlayers;
+	
+	private static final int NUMBEROFPLAYERS = 4;
+	
+	public Training(){
+		
+	}
+	
+	public Training(User trainer, Court court, Calendar startDate,Calendar endDate){
+		this.trainer= trainer;
+		this.court = court;
+		this.startDate = startDate;
+		this.endDate= endDate;
+		this.players= new ArrayList<User>();
+		this.numberOfPlayers = NUMBEROFPLAYERS;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public User getTrainer() {
+		return trainer;
+	}
+
+	public List<User> getPlayers() {
+		return players;
+	}
+
+	public Court getCourt() {
+		return court;
+	}
+
+	public Calendar getStartDate() {
+		return startDate;
+	}
+
+	public Calendar getEndDate() {
+		return endDate;
+	}
+	
+	public int getNumberOfPlayers() {
+		return numberOfPlayers;
+	}
+
+	@Override
+	public int hashCode() {
+		return id;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Training other = (Training) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Training [id=" + id + ", trainer=" + trainer + ", players=" + players + ", court=" + court
+				+ ", startDate=" + startDate.getTime() + ", endDate=" + endDate.getTime() + "]";
+	}
+	
+	public boolean addPlayer(User player){
+		if(this.players.size() < this.numberOfPlayers){
+			return this.players.add(player);
+		}
+		else{
+			return false;
+		}
+	}
+	
+	public boolean removePlayer(User player){
+		return this.players.remove(player);
+	}
+}
