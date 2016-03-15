e87b90e438370892df16acc92d16e10cd2ca53b8
LigiaCano <ligiacanorengifo@gmail.com>
2016-03-15 09:19:12 +0100

    Refactoring of users with non expired token

--- src/main/java/data/daos/UserDao.java
+++ src/main/java/data/daos/UserDao.java
@@ -1,5 +1,7 @@
 package data.daos;
 
+import java.util.Calendar;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -12,5 +14,11 @@ public interface UserDao extends JpaRepository<User, Integer> {
 
     @Query("select user from User user where user.username = ?1 or user.email = ?1")
     public User findByUsernameOrEmail(String id);
+    
+    @Query("select token.user from Token token where token.value = ?1 and token.expirationDate >= ?2")
+    public User findByTokenValueNonExpiredToken(String tokenValue, Calendar date);
+
+    @Query("select token.user from Token token join token.user user  where (user.username = ?1 or user.email = ?1) and token.expirationDate >= ?2")
+    public User findByUsernameOrEmailNonExpiredToken(String id, Calendar date);
 
 }
--- src/main/java/data/services/UserDetailsServiceImpl.java
+++ src/main/java/data/services/UserDetailsServiceImpl.java
@@ -2,6 +2,7 @@ package data.services;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.List;
 
 import javax.transaction.Transactional;
@@ -31,18 +32,19 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     private AuthorizationDao authorizationDao;
 
     @Override
-    public UserDetails loadUserByUsername(final String username) throws UsernameNotFoundException {
-        User user = userDao.findByTokenValue(username);
-        if (user == null) {
-            user = userDao.findByUsernameOrEmail(username);
-            if (user == null) {
-                throw new UsernameNotFoundException("Usuario no encontrado");
-            } else {
-                return this.userBuilder(user.getUsername(), user.getPassword(), Arrays.asList(Role.AUTHENTICATED));
-            }
-        } else {
+    public UserDetails loadUserByUsername(final String usernameOrEmailOrTokenValue) throws UsernameNotFoundException {
+        Calendar date = Calendar.getInstance();
+    	User user = userDao.findByTokenValueNonExpiredToken(usernameOrEmailOrTokenValue, date);
+        if (user != null) {
             List<Role> roleList = authorizationDao.findRoleByUser(user);
             return this.userBuilder(user.getUsername(), new BCryptPasswordEncoder().encode(""), roleList);
+        } else {
+            user = userDao.findByUsernameOrEmailNonExpiredToken(usernameOrEmailOrTokenValue, date);
+            if (user != null) {
+                return this.userBuilder(user.getUsername(), user.getPassword(), Arrays.asList(Role.AUTHENTICATED));
+            } else {
+                throw new UsernameNotFoundException("Usuario no encontrado");
+            }
         }
     }
 
