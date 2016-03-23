4e9727e8929793b08ca6fee917efd5ff82db9ccb
LigiaCano <ligiacanorengifo@gmail.com>
2016-03-23 16:29:21 +0100

    Controller training test

---
+++ src/test/java/business/controllers/TrainingControllerTest.java
@@ -0,0 +1,48 @@
+package business.controllers;
+
+import static org.junit.Assert.*;
+
+import java.util.Calendar;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import business.wrapper.CreateTraining;
+import config.PersistenceConfig;
+import config.TestsPersistenceConfig;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = { PersistenceConfig.class, TestsPersistenceConfig.class })
+public class TrainingControllerTest {
+
+	@Autowired
+	private TrainingController trainingController;
+
+	@Test
+	public void testCreateTraining() {
+		CreateTraining createTraining = new CreateTraining(Calendar.getInstance(), 1, 3);
+		Calendar starDate = createTraining.getStartDate();
+		starDate.add(Calendar.DAY_OF_YEAR, 2);
+		starDate.set(Calendar.HOUR_OF_DAY, 10);
+		starDate.set(Calendar.MINUTE, 0);
+		starDate.set(Calendar.SECOND, 0);
+		starDate.set(Calendar.MILLISECOND, 0);
+		Calendar endDate = (Calendar) starDate.clone();
+		endDate.add(Calendar.WEEK_OF_YEAR, createTraining.getNumberWeek() - 1);
+		endDate.add(Calendar.HOUR_OF_DAY, 1);
+		createTraining.setTrainerUsername("trainer");
+		createTraining.setEndDate(endDate);
+		assertFalse(trainingController.createTraining(createTraining));
+	}
+	
+	@Test
+	public void testShowTrainings() {
+		Calendar date = Calendar.getInstance();
+		System.out.println(date);
+		assertEquals(trainingController.showTrainings(date).size(),1);
+	}
+
+}
