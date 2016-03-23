dd417b025b0a43cd9d4a2c31823191684fa515fb
LigiaCano <ligiacanorengifo@gmail.com>
2016-03-23 16:27:42 +0100

    Refactoring method findByStartDate

--- src/main/java/data/daos/TrainingDao.java
+++ src/main/java/data/daos/TrainingDao.java
@@ -4,6 +4,7 @@ import java.util.Calendar;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import data.entities.Court;
 import data.entities.Training;
@@ -13,6 +14,7 @@ public interface TrainingDao extends JpaRepository<Training, Integer> {
 	
 	Training findById(int id);
 	
+	@Query("select training from Training training where training.startDate >=?1")
 	List<Training> findByStartDate(Calendar date);
 	
 	List<Training> findByTrainer(User trainer);
