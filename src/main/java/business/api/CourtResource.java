d35d252c390b304a1611750bd087c1fade1be0f1
LigiaCano <ligiacanorengifo@gmail.com>
2017-01-05 12:32:26 +0100

    Refactoring

--- src/main/java/business/api/CourtResource.java
+++ src/main/java/business/api/CourtResource.java
@@ -2,8 +2,6 @@ package business.api;
 
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,48 +12,44 @@ import org.springframework.web.bind.annotation.RestController;
 import business.api.exceptions.AlreadyExistCourtIdException;
 import business.api.exceptions.NotFoundCourtIdException;
 import business.controllers.CourtController;
-import business.controllers.SocialController;
 import business.wrapper.CourtState;
 
 @RestController
 @RequestMapping(Uris.SERVLET_MAP + Uris.COURTS)
 public class CourtResource {
 
-    private CourtController courtController;
-    private SocialController socialController;
+	private CourtController courtController;
 
-    @Autowired
-    public void setCourtController(CourtController courtController, SocialController socialController) {
-        this.courtController = courtController;
-        this.socialController = socialController;
-    }
+	@Autowired
+	public void setCourtController(CourtController courtController) {
+		this.courtController = courtController;
+	}
 
-    @RequestMapping(method = RequestMethod.POST)
-    public void createCourt(@RequestParam(required = true) int id) throws AlreadyExistCourtIdException {
-        if (!this.courtController.createCourt(id)) {
-            throw new AlreadyExistCourtIdException();
-        }
-        socialController.postFacebook("Creada pista Nº: " + id);
-        socialController.postTwitter("Creada pista Nº: " + id);
-    }
+	@RequestMapping(method = RequestMethod.POST)
+	public void createCourt(@RequestParam(required = true) int id) throws AlreadyExistCourtIdException {
+		if (!this.courtController.createCourt(id)) {
+			throw new AlreadyExistCourtIdException();
+		}
 
-    @RequestMapping(value = Uris.ID + Uris.ACTIVE, method = RequestMethod.POST)
-    public void changeCourtActivationTrue(@PathVariable int id) throws NotFoundCourtIdException {
-        if (!courtController.changeCourtActivation(id, true)) {
-            throw new NotFoundCourtIdException("id: " + id);
-        }
-    }
+	}
 
-    @RequestMapping(value = Uris.ID + Uris.ACTIVE, method = RequestMethod.DELETE)
-    public void changeCourtActivationFalse(@PathVariable int id) throws NotFoundCourtIdException {
-        if (!courtController.changeCourtActivation(id, false)) {
-            throw new NotFoundCourtIdException("id: " + id);
-        }
-    }
+	@RequestMapping(value = Uris.ID + Uris.ACTIVE, method = RequestMethod.POST)
+	public void changeCourtActivationTrue(@PathVariable int id) throws NotFoundCourtIdException {
+		if (!courtController.changeCourtActivation(id, true)) {
+			throw new NotFoundCourtIdException("id: " + id);
+		}
+	}
 
-    @RequestMapping(method = RequestMethod.GET)
-    public List<CourtState> showCourts() {
-        return courtController.showCourts();
-    }
+	@RequestMapping(value = Uris.ID + Uris.ACTIVE, method = RequestMethod.DELETE)
+	public void changeCourtActivationFalse(@PathVariable int id) throws NotFoundCourtIdException {
+		if (!courtController.changeCourtActivation(id, false)) {
+			throw new NotFoundCourtIdException("id: " + id);
+		}
+	}
+
+	@RequestMapping(method = RequestMethod.GET)
+	public List<CourtState> showCourts() {
+		return courtController.showCourts();
+	}
 
 }
